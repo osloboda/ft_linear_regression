a01e2d3f42a9ab4048cce9334ca322af01c9d47f
Oleh SLOBODANIUK <osloboda@e1r6p9.unit.ua>
2020-02-05 19:50:38 +0200

    recode rmse eval

--- training.py
+++ training.py
@@ -1,8 +1,12 @@
 import csv
 import matplotlib.pyplot as plt
-from math import sqrt
-LR = 0.2
 EPS = 0.0002
+LR = 0.2
+theta0, theta1 = 0.0, 0.0
+
+
+def estimatePrice(x):
+    return theta0 + theta1 * x
 
 
 def getData(filename):
@@ -23,72 +27,70 @@ def rmse_metric(actual, predicted):
         prediction_error = predicted[i] - float(actual[i])
         sum_error += (prediction_error ** 2)
     mean_error = sum_error / float(len(actual))
-    return sqrt(mean_error)
+    return mean_error / mean_error
 
 
-def simple_linear_regression(test, theta0, theta1):
+def simple_predict(test):
     predictions = list()
     for row in test:
-        yhat = theta0 + theta1 * float(row[0])
-        predictions.append(yhat)
+        predictions.append(theta0 + theta1 * float(row[0]))
     return predictions
 
 
-def estimatePrice(x, theta0, theta1):
-    return theta0 + theta1 * x
-
-
-def gradient(data, theta0, theta1, max0, min0):
-    costfunction, cycles = 0, 0
-    previouscost = 10 ** 6
+def scale(data):
+    max0 = max(data)[0]
+    min0 = min(data)[0]
     scaleddata = []
-    m = float(len(data))
     for elem in data:
         new = [(elem[0] / (max0 - min0)), elem[1]]
         scaleddata.append(new)
-    while abs(costfunction - previouscost) > EPS:
-        previouscost = costfunction
+    return scaleddata
+
+
+def gradient(data, max0, min0):
+    global theta0, theta1
+    cost, cycles = 0, 0
+    previouscost = 1
+
+
+    m = float(len(data))
+
+    while abs(cost - previouscost) > EPS:
+        previouscost = cost
         sigma, tmp0, tmp1 = 0.0, 0.0, 0.0
-        for row in scaleddata:
-            sigma += estimatePrice(row[0], theta0, theta1) - row[1]
+        for row in data:
+            sigma += estimatePrice(row[0]) - row[1]
             tmp0 += sigma
             tmp1 += sigma * row[0]
         theta0 = theta0 - LR / m * (tmp0 / m)
         theta1 = theta1 - LR / m * (tmp1 / m)
-        costfunction = 1 / m * (tmp0 * tmp0)
+        cost = 1 / m * (tmp0 * tmp0)
         cycles += 1
     print("Cycles: ", cycles)
     theta1 /= max0 - min0
-    return theta0, theta1
 
 
 data = getData('data.csv')
-theta0, theta1 = 0.0, 0.0
 
-# bonus part
-max0 = max(data)[0]
-min0 = min(data)[0]
+gradient(scale(data), max(data)[0], min(data)[0])
 
-(theta0, theta1) = gradient(data, theta0, theta1, max0, min0)
-
-test_set = list()
-for row in data:
-    row_copy = list(row)
-    row_copy[-1] = None
-    test_set.append(row_copy)
-predicted = simple_linear_regression(test_set, theta0, theta1)
-actual = [row[-1] for row in data]
-
-print('Precision: %.3f' % (rmse_metric(actual, predicted)))
-print(theta0, theta1)
 f = open('scales.csv', 'w')
 f.write(str(theta0) + ", " + str(theta1))
 f.close()
-fig, ax = plt.subplots()
 
+test_set = list()
+for row in data:
+    row_copy = list(row)                                # evaluate RMSE
+    row_copy[-1] = None
+    test_set.append(row_copy)
+predicted = simple_predict(test_set)
+actual = [row[-1] for row in data]
+print('RMSE: %.3f' % (rmse_metric(actual, predicted)))
+print(theta0, theta1)
+
+fig, ax = plt.subplots()
 ax.scatter([int(row[0]) for row in data], [int(row[1]) for row in data])
 plt.plot([float(row[0]) for row in data], predicted, "r")
-
 ax.set_xlabel('km')
-ax.set_ylabel('price')
+ax.set_ylabel('price')                                  # plot building
 plt.show()
