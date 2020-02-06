213d6d97874b3830c0f709f51f6891037aa980ad
Oleh SLOBODANIUK <osloboda@e1r6p9.unit.ua>
2020-02-06 18:44:30 +0200

    add -s flag (turn on analytics mode) to argparse

--- training.py
+++ training.py
@@ -6,6 +6,7 @@ import argparse
 EPS = 0.0002
 LR = 0.2
 theta0, theta1 = 0.0, 0.0
+stat = False
 
 
 def estimatePrice(x):
@@ -22,6 +23,12 @@ def getData(filename):
             row1 = float(row[1])
             data.add((row0, row1))
     return data
+
+
+class turn_stat(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        global stat
+        stat = True
 
 
 def rmse_metric(actual, predicted):
@@ -50,63 +57,68 @@ def scale(data):
     return scaleddata
 
 
-def gradient(data, max0, min0):
+def evaluete_thetas_cost(data):
+    sigma, temp_theta0, temp_theta1, m = 0.0, 0.0, 0.0, len(data)
+
+    for row in data:
+        sigma += estimatePrice(row[0]) - row[1]
+        temp_theta0 += sigma
+        temp_theta1 += sigma * row[0]
+
+    return theta0 - LR / m * (temp_theta0 / m),\
+           theta1 - LR / m * (temp_theta1 / m),\
+           1 / m * (temp_theta0 * temp_theta0)
+
+
+def gradient(data):
     global theta0, theta1
-    cost, cycles = 0, 0
-    previouscost = 1
-
-
-    m = float(len(data))
+    cost, cycles, previouscost = 0, 0, 1
 
     while abs(cost - previouscost) > EPS:
         previouscost = cost
-        sigma, tmp0, tmp1 = 0.0, 0.0, 0.0
-        for row in data:
-            sigma += estimatePrice(row[0]) - row[1]
-            tmp0 += sigma
-            tmp1 += sigma * row[0]
-        theta0 = theta0 - LR / m * (tmp0 / m)
-        theta1 = theta1 - LR / m * (tmp1 / m)
-        cost = 1 / m * (tmp0 * tmp0)
+        theta0, theta1, cost = evaluete_thetas_cost(data)
         cycles += 1
-    print("Cycles: ", cycles)
-    theta1 /= max0 - min0
 
-class StartAction(argparse.Action):
-    def __call__(self, parser, namespace, values, option_string=None):
-        pass
-
-parser = argparse.ArgumentParser(description='Get arguments')
-parser.add_argument('-f', action='store', dest='f', help='Input file name')
-parser.add_argument('-s', action=StartAction ,nargs=0, dest='s', help='Input file name')
-args = parser.parse_args()
-
-if not args.f or not os.path.exists(args.f):
-    print("usage: training.py [-h] [-f] \n positional arguments:  -f     Input file name \n\noptional arguments: -h for help")
-    exit()
-
-data = getData('data.csv')
-
-gradient(scale(data), max(data)[0], min(data)[0])
-
-f = open('scales.csv', 'w')
-f.write(str(theta0) + ", " + str(theta1))
-f.close()
+    if stat:
+        print("Cycles: ", cycles)
 
 
-test_set = list()
-for row in data:
-    row_copy = list(row)                                # evaluate RMSE
-    row_copy[-1] = None
-    test_set.append(row_copy)
-predicted = simple_predict(test_set)
-actual = [row[-1] for row in data]
-print('RMSE: %.3f' % (rmse_metric(actual, predicted)))
-print(theta0, theta1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Get arguments')
+    parser.add_argument('-f', action='store', dest='f', help='Input file name')
+    parser.add_argument('-s', action=turn_stat, nargs=0)
+    args = parser.parse_args()
 
-fig, ax = plt.subplots()
-ax.scatter([int(row[0]) for row in data], [int(row[1]) for row in data])
-plt.plot([float(row[0]) for row in data], predicted, "r")
-ax.set_xlabel('km')
-ax.set_ylabel('price')                                  # plot building
-plt.show()
+    if not args.f or not os.path.exists(args.f):
+        print("usage: training.py [-h] [-f] \n positional arguments:  -f     Input file name \n                      "
+              "  -s     Dispay info about training\n optional arguments:    -h     for help")
+        exit()
+
+    data = getData(args.f)
+    max0, min0 = max(data)[0], min(data)[0]
+
+    gradient(scale(data))
+
+    theta1 /= max0 - min0                                       # unscale thetha after eval
+
+    print(theta0, theta1)
+    f = open('scales.csv', 'w')
+    f.write(str(theta0) + ", " + str(theta1))
+    f.close()
+
+    if stat:
+        test_set = list()
+        for row in data:
+            row_copy = list(row)                                # evaluate RMSE
+            row_copy[-1] = None
+            test_set.append(row_copy)
+        predicted = simple_predict(test_set)
+        actual = [row[-1] for row in data]
+        print('RMSE: %.3f' % (rmse_metric(actual, predicted)))
+
+        fig, ax = plt.subplots()
+        ax.scatter([int(row[0]) for row in data], [int(row[1]) for row in data])
+        plt.plot([float(row[0]) for row in data], predicted, "r")
+        ax.set_xlabel('km')
+        ax.set_ylabel('price')                                  # plot building
+        plt.show()
